28e4acd29d6371a32ae968ff14ef9d8d4918aebc
OneEyedPheonix <ikebernal2003@gmail.com>
2026-04-18 12:34:44 -0700

    Update main.c

--- Modular Sensor code/main.c
+++ Modular Sensor code/main.c
@@ -81,7 +81,7 @@ int main(void) {
     //Thermister paramaters
     thermistor_cfg_t tcfg = {
         .vref = ADC_VREF,
-        .r_fixed_ohms = 1500000.0,      
+        .r_fixed_ohms = 100000.0,      
         .r0_ohms = 100000.0,           
         .t0_c = 25.0,
         .beta = 3950.0                
@@ -90,18 +90,33 @@ int main(void) {
     // Transducer Parameters
     transducer_cfg_t icfg = {
         .vref = ADC_VREF,
-        .shunt_ohms = 165.0,          // example: 165Ω => 20mA -> 3.3V
+        .shunt_ohms = 150.0,          
         .ma_min = 4.0,
         .ma_max = 20.0,
-        .eng_min = 0.0,               // engineering units (e.g., PSI) min
-        .eng_max = 100.0              // engineering units max
+        .eng_min = 0.0,               // engineering units min(Actual units)
+        .eng_max = 300.0              // engineering units max
+    };
+
+    transducer_cfg_t icfg2 = {
+    .vref = ADC_VREF,
+    .shunt_ohms = 150.0,
+    .ma_min = 4.0,
+    .ma_max = 20.0,
+    .eng_min = 0.0,
+    .eng_max = 30.0
+};
+
+    const transducer_cfg_t *TRANS_CFGS[] = {
+    &icfg,
+    &icfg,
+    &icfg2
     };
 
     // Voltage divider scaling
     voltage_divider_cfg_t vcfg = {
         .vref = ADC_VREF,
-        .r_top_ohms = 100000.0,       
-        .r_bot_ohms = 10000.0         
+        .r_top_ohms = 1500000.0,       
+        .r_bot_ohms = 100000.0         
     };
      //ADC Assignments
      // ADC0 assignments
@@ -114,11 +129,11 @@ int main(void) {
     adc_input_t THERM7  = { fd_adc0, 6 };
     adc_input_t THERM8  = { fd_adc0, 7 };
     //ADC1 assignments
-    adc_input_t THERM9 =  { fd_adc1, 0 }; //Two thermistors on second adc
-    adc_input_t THERM10 = { fd_adc1, 1 };
-    adc_input_t TRANS1 =  { fd_adc1, 2 };//Three transducers
-    adc_input_t TRANS2 =  { fd_adc1, 3 };
-    adc_input_t TRANS3 =  { fd_adc1, 4 };
+    adc_input_t THERM9 =  { fd_adc1, 6 }; //Two thermistors on second adc
+    adc_input_t THERM10 = { fd_adc1, 7 };
+    adc_input_t TRANS1 =  { fd_adc1, 5 }; //Three transducers
+    adc_input_t TRANS2 =  { fd_adc1, 4 };
+    adc_input_t TRANS3 =  { fd_adc1, 3 };
     //ADC2 assignments
     adc_input_t VOLTS1  = { fd_adc2, 0 };
     adc_input_t VOLTS2  = { fd_adc2, 1 };
@@ -126,7 +141,7 @@ int main(void) {
     adc_input_t VOLTS4  = { fd_adc2, 3 };
     adc_input_t VOLTS5  = { fd_adc2, 4 };
     adc_input_t VOLTS6  = { fd_adc2, 5 };
-    adc_input_t VOLTS6  = { fd_adc2, 6 };
+    adc_input_t VOLTS7  = { fd_adc2, 6 };
 
     //Store Thermistor values in array
     adc_input_t THERMS[] = {
@@ -140,9 +155,10 @@ int main(void) {
     const int N_TRANS = (int)(sizeof(TRANS) / sizeof(TRANS[0])); 
     adc_input_t VOLTS[] = {
     VOLTS1, VOLTS2, VOLTS3, VOLTS4, VOLTS5,
-    VOLTS6
+    VOLTS6, VOLTS7
     };
     const int N_VOLTS = (int)(sizeof(VOLTS) / sizeof(VOLTS[0])); 
+
     // ---- Main loop ----
     const useconds_t loop_us = (useconds_t)(1000000.0 / LOOP_HZ);
 
@@ -165,9 +181,9 @@ int main(void) {
         printf("---- TRANSDUCERS ----\n");
         for (int i = 0; i < N_TRANS; i++) {
         int adc = adc_read(&TRANS[i]);
-        double current = transducer_from_adc(adc, &icfg);
-        double eng = transducer_eng_from_ma(current, &icfg);
-        printf("TRANS%02d | adc=%4d | mA=%6.2f | Eng=%6.2f\n", i + 1, adc, current, eng);
+        double loopcurrent = transducer_from_adc(adc, TRANS_CFGS[i]);
+        double actcurrent = transducer_eng_from_ma(loopcurrent, TRANS_CFGS[i]);
+        printf("TRANS%02d | adc=%4d | mA=%6.2f | Eng=%6.2f\n", i + 1, adc, loopcurrent, actcurrent);
         }
         printf("---- VOLTAGES ----\n");
         for (int i = 0; i < N_VOLTS; i++) {
@@ -188,7 +204,7 @@ int main(void) {
         usleep(loop_us);
     }
 
-    // unreachable, but tidy
+    // unreachable but tidy
     mpu6050_close(imu_fd);
     if (fd_adc0 >= 0) mcp3008_close(fd_adc0);
     if (fd_adc1 >= 0) mcp3008_close(fd_adc1);
